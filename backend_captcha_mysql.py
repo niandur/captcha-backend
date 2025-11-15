eee32b6dc4ff6ec30d8b4a46ddc9e543533e5ae6
niandur <evallovetc@gmail.com>
2025-11-15 13:06:12 +0100

    Add traceback printing to diagnose 500 error

--- backend_captcha_mysql.py
+++ backend_captcha_mysql.py
@@ -73,4 +73,6 @@ def guardar_captcha():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
