adcaea31094a290df9e99a654d0b83af5b5070d5
niandur <evallovetc@gmail.com>
2025-11-15 12:15:44 +0100

    FIX: Remove debug and use PORT from Render

--- backend_captcha_mysql.py
+++ backend_captcha_mysql.py
@@ -137,4 +137,6 @@ def guardar_captcha():
 # ---------------------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
